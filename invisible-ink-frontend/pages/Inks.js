67fc87f8048cd4b2fc3327e69c0543350ab56ec2
Tim <timg51237@gmail.com>
2020-02-22 12:38:30 -0800

    school stuff

--- invisible-ink-frontend/pages/Inks.js
+++ invisible-ink-frontend/pages/Inks.js
@@ -1,133 +1,158 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import ChatBox from "../components/ChatBox";
-import { Tabs, TabPane, Button } from "antd";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import ChatBox from '../components/ChatBox';
+import { Tabs, TabPane, Button, Select, Input } from 'antd';
 
 class Inks extends Component {
-  static async getInitialProps({ store }) {
-    const inks = [
-      {
-        0: "ink 1",
-        messsage: messageSample
-      },
-      {
-        0: "ink 2",
-        message: messageSample
-      }
-    ];
-    return { inks };
-  }
+    static async getInitialProps({ store }) {
+        const inks = [
+            {
+                0: 'ink 1',
+                messsage: messageSample
+            },
+            {
+                0: 'ink 2',
+                message: messageSample
+            }
+        ];
+        const schools = ['Sage', 'Uni', 'etc...'];
 
-  state = {
-    text: "",
-    highlighted: "-1"
-  };
+        return { inks, schools };
+    }
 
-  handleSubmit = () => {
-    console.log("handling submit");
-  };
+    state = {
+        text: '',
+        highlighted: '-1',
+        school: '',
+        schoolInput: ''
+    };
 
-  renderInk() {
-    return (
-      <div style={{ width: "70vw", height: "100%" }}>
-        <h2> What's Wrong?</h2>
-        <ChatBox
-          messages={messageSample}
-          address={123}
-          handleSubmit={this.handleSubmit}
-          changeInput={event => this.setState({ text: event.target.value })}
-        />
-      </div>
-    );
-  }
+    handleSubmit = () => {
+        console.log('handling submit');
+    };
 
-  render() {
-    const { TabPane } = Tabs;
+    renderInk() {
+        return (
+            <div style={{ width: '70vw', height: '100%' }}>
+                <h2> What's Wrong?</h2>
+                <ChatBox
+                    messages={messageSample}
+                    address={123}
+                    handleSubmit={this.handleSubmit}
+                    changeInput={event => this.setState({ text: event.target.value })}
+                />
+            </div>
+        );
+    }
 
-    return (
-      <div style={{ width: "90vw", margin: "5vw" }}>
-        <h1 style={{ textAlign: "center" }}> Inks </h1>
-        <div style={{ display: "flex", flexDirection: "row", width: "100%" }}>
-          <Tabs
-            tabPosition="left"
-            onChange={key => this.setState({ highlighted: key })}
-            activeKey={this.state.highlighted}
-          >
-            {this.props.inks
-              ? this.props.inks.map((object, index) => (
-                  <TabPane
-                    key={index}
-                    tab={
-                      <div
-                        style={{
-                          width: "15vw",
-                          padding: "0.5vw",
-                          overflowWrap: "normal",
-                          whiteSpace: "normal"
-                        }}
-                      >
-                        <div
-                          style={{
-                            fontSize: "150%"
-                          }}
-                        >
-                          {object[0]}
-                        </div>
-                      </div>
-                    }
-                  >
-                    {this.renderInk(index)}
-                    {console.log(object)}
-                  </TabPane>
-                ))
-              : null}
-            <TabPane
-              key={-1}
-              disabled
-              tab={
-                <div
-                  style={{
-                    width: "15vw",
-                    padding: "0.5vw",
-                    overflowWrap: "normal",
-                    whiteSpace: "normal"
-                  }}
+    renderSchool() {
+        const { Option } = Select;
+        return (
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+                <Select
+                    style={{ width: '300px' }}
+                    placeholder="Select a School"
+                    onChange={val => this.setState({ school: val })}
                 >
-                  <Button style={{ margin: "5%", width: "90%" }} type="primary">
-                    Create New Ink
-                  </Button>
+                    {this.props.schools.map(school => (
+                        <Option value={school}> {school}</Option>
+                    ))}
+                </Select>
+                <Input
+                    style={{ width: '200px' }}
+                    onChange={event => this.setState({ schoolInput: event.target.value })}
+                ></Input>
+                <Button type="primary" onClick={() => console.log('adding school here')}>
+                    Add School
+                </Button>
+            </div>
+        );
+    }
+
+    render() {
+        const { TabPane } = Tabs;
+        return (
+            <div style={{ width: '90vw', margin: '5vw' }}>
+                <h1 style={{ textAlign: 'center' }}> Inks </h1>
+                {this.props.user && this.props.user.administrator ? this.renderSchool() : null}
+                <div style={{ display: 'flex', flexDirection: 'row', width: '100%' }}>
+                    <Tabs
+                        tabPosition="left"
+                        onChange={key => this.setState({ highlighted: key })}
+                        activeKey={this.state.highlighted}
+                    >
+                        {this.props.inks
+                            ? this.props.inks.map((object, index) => (
+                                  <TabPane
+                                      key={index}
+                                      tab={
+                                          <div
+                                              style={{
+                                                  width: '15vw',
+                                                  padding: '0.5vw',
+                                                  overflowWrap: 'normal',
+                                                  whiteSpace: 'normal'
+                                              }}
+                                          >
+                                              <div
+                                                  style={{
+                                                      fontSize: '150%'
+                                                  }}
+                                              >
+                                                  {object[0]}
+                                              </div>
+                                          </div>
+                                      }
+                                  >
+                                      {this.renderInk(index)}
+                                      {console.log(object)}
+                                  </TabPane>
+                              ))
+                            : null}
+                        <TabPane
+                            key={-1}
+                            disabled
+                            tab={
+                                <div
+                                    style={{
+                                        width: '15vw',
+                                        padding: '0.5vw',
+                                        overflowWrap: 'normal',
+                                        whiteSpace: 'normal'
+                                    }}
+                                >
+                                    <Button style={{ margin: '5%', width: '90%' }} type="primary">
+                                        Create New Ink
+                                    </Button>
+                                </div>
+                            }
+                        >
+                            <h1
+                                style={{
+                                    textAlign: 'center',
+                                    width: '100%',
+                                    marginTop: '1.5vw'
+                                }}
+                            >
+                                Select an Ink
+                            </h1>
+                        </TabPane>
+                    </Tabs>
                 </div>
-              }
-            >
-              <h1
-                style={{
-                  textAlign: "center",
-                  width: "100%",
-                  marginTop: "1.5vw"
-                }}
-              >
-                Select an Ink
-              </h1>
-            </TabPane>
-          </Tabs>
-        </div>
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 const messageSample = [
-  {
-    0: "123",
-    1: "message text"
-  },
-  {
-    0: "456",
-    1: "message text 2"
-  }
+    {
+        0: '123',
+        1: 'message text'
+    },
+    {
+        0: '456',
+        1: 'message text 2'
+    }
 ];
 
-export default connect(
-  null,
-  null
-)(Inks);
+export default connect(null, null)(Inks);
