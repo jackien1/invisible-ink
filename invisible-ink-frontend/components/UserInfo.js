03e434872d05f6e794cdb7b77f31088a2a674482
jackien1 <jackieni2002@gmail.com>
2020-02-22 11:29:36 -0800

    ok

--- invisible-ink-frontend/components/UserInfo.js
+++ invisible-ink-frontend/components/UserInfo.js
@@ -1,156 +1,146 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 //import { setCurrentUser, logoutUser, loginUser } from '../redux/actions/auth_actions';
-import { Divider, Icon, Button, Input } from 'antd';
-import { Router } from '../routes';
-import axios from 'axios';
-import Web3 from 'web3';
+import { Divider, Icon, Button, Input } from "antd";
+import { Router } from "../routes";
+import axios from "axios";
+import Web3 from "web3";
 
 class PlayerInfo extends Component {
-    state = {
-        dcBalance: '0',
-        ethBalance: '0',
-        value: 0,
-        expand: false,
-        denom: 'ether',
-        email: '',
-        password: '',
-        loading: false
+  state = {
+    dcBalance: "0",
+    ethBalance: "0",
+    value: 0,
+    expand: false,
+    email: "",
+    password: "",
+    loading: false
+  };
+
+  submit = async () => {
+    const user = {
+      email: this.state.email,
+      password: this.state.password
     };
+    this.props.loginUser(user, this.callback);
+  };
 
-    submit = async () => {
-        const user = {
-            email: this.state.email,
-            password: this.state.password
-        };
-        this.props.loginUser(user, this.callback);
-    };
+  callback = () => {
+    Router.push("/cases");
+  };
 
-    callback = () => {
-        Router.push('/cases');
-    };
+  renderNotLoggedIn() {
+    console.log(this.props.error);
+    return (
+      <div
+        style={{
+          padding: "10px",
+          display: "flex",
+          flexDirection: "column"
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            textAlign: "center"
+          }}
+        >
+          <h2> Log In </h2>
+          <Input
+            style={{ marginBottom: "10px" }}
+            placeholder="Email"
+            onChange={event =>
+              this.setState({
+                email: event.target.value
+              })
+            }
+          />
+          <Input
+            placeholder="Password"
+            type="password"
+            onChange={event =>
+              this.setState({
+                password: event.target.value
+              })
+            }
+          />
+          <Button
+            type="primary"
+            style={{ margin: "20px 20px 0px 20px" }}
+            onClick={this.submit}
+            loading={this.state.loading}
+          >
+            Log In
+          </Button>
 
-    // addEther = async () => {
-    //     const web3 = new Web3('https://rinkeby.infura.io/v3/6a20903e63ec4a96a771a79800a1a1d4');
-
-    //     try {
-    //         await axios({
-    //             method: 'post',
-    //             url: `${process.env.SERVER_URL}/api/gateway/addBalance`,
-    //             data: {
-    //                 username: this.props.user.username,
-    //                 value: web3.utils.toWei(this.state.value.toString(), this.state.denom)
-    //             }
-    //         });
-    //     } catch (e) {
-    //         console.log(e);
-    //     }
-    // };
-
-    renderNotLoggedIn() {
-        console.log(this.props.error);
-        return (
-            <div
-                style={{
-                    padding: '10px',
-                    display: 'flex',
-                    flexDirection: 'column'
-                }}
+          {this.props.error ? (
+            <h3
+              style={{
+                textAlign: "center",
+                color: "red",
+                margin: "10px 0px -10px 0px"
+              }}
             >
-                <div
-                    style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'center',
-                        textAlign: 'center'
-                    }}
-                >
-                    <h2> Log In </h2>
-                    <Input
-                        style={{ marginBottom: '10px' }}
-                        placeholder="Email"
-                        onChange={event =>
-                            this.setState({
-                                email: event.target.value
-                            })
-                        }
-                    />
-                    <Input
-                        placeholder="Password"
-                        type="password"
-                        onChange={event =>
-                            this.setState({
-                                password: event.target.value
-                            })
-                        }
-                    />
-                    <Button
-                        type="primary"
-                        style={{ margin: '20px 20px 0px 20px' }}
-                        onClick={this.submit}
-                        loading={this.state.loading}
-                    >
-                        Log In
-                    </Button>
+              Login Error!
+            </h3>
+          ) : null}
+        </div>
 
-                    {this.props.error ? (
-                        <h3
-                            style={{
-                                textAlign: 'center',
-                                color: 'red',
-                                margin: '10px 0px -10px 0px'
-                            }}
-                        >
-                            Login Error!
-                        </h3>
-                    ) : null}
-                </div>
+        <Divider />
+        <Button
+          style={{ margin: "0px 20px 0 20px" }}
+          onClick={() => Router.push("/register/student")}
+        >
+          Register (Student)
+        </Button>
+        <Button
+          style={{ margin: "0px 20px 0 20px" }}
+          onClick={() => Router.push("/register/admin")}
+        >
+          Register (Administrator)
+        </Button>
+      </div>
+    );
+  }
 
-                <Divider />
-                <Button
-                    style={{ margin: '0px 20px 0 20px' }}
-                    onClick={() => Router.push('/register/student')}
-                >
-                    Register (Student)
-                </Button>
-                <Button
-                    style={{ margin: '0px 20px 0 20px' }}
-                    onClick={() => Router.push('/register/admin')}
-                >
-                    Register (Administrator)
-                </Button>
-            </div>
-        );
-    }
+  renderLoggedIn() {
+    const web3 = new Web3(
+      "https://rinkeby.infura.io/v3/6a20903e63ec4a96a771a79800a1a1d4"
+    );
 
-    renderLoggedIn() {
-        const web3 = new Web3('https://rinkeby.infura.io/v3/6a20903e63ec4a96a771a79800a1a1d4');
+    const full = (
+      <div
+        style={{
+          padding: "10px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center"
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center"
+          }}
+        >
+          <span style={{ fontSize: "120%" }}> Signed in as </span>
+          <span
+            style={{
+              fontSize: "150%",
+              color: "#10004e",
+              fontWeight: "600"
+            }}
+          >
+            {this.props.user.name}
+          </span>
+        </div>
 
-        const full = (
-            <div
-                style={{
-                    padding: '10px',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center'
-                }}
-            >
-                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                    <span style={{ fontSize: '120%' }}> Signed in as </span>
-                    <span
-                        style={{
-                            fontSize: '150%',
-                            color: '#10004e',
-                            fontWeight: '600'
-                        }}
-                    >
-                        {this.props.user.name}
-                    </span>
-                </div>
+        <Divider />
 
-                <Divider />
-
-                {/* <div
+        {/* <div
                     style={{
                         display: 'flex',
                         flexDirection: 'column',
@@ -196,51 +186,56 @@ class PlayerInfo extends Component {
                     </div>
                 </div> */}
 
-                <div
-                    style={{
-                        display: 'flex',
-                        flexWrap: 'wrap',
-                        justifyContent: 'center'
-                    }}
-                >
-                    {this.links.map(link => (
-                        <Button
-                            icon={link.icon}
-                            onClick={link.click}
-                            style={{ width: '120px', margin: '5px' }}
-                        >
-                            {link.text}
-                        </Button>
-                    ))}
-                </div>
-                <Button
-                    icon="logout"
-                    onClick={() => this.props.logoutUser(() => Router.push('/'))}
-                    type="danger"
-                    style={{ width: '220px' }}
-                >
-                    Logout
-                </Button>
-            </div>
-        );
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center"
+          }}
+        >
+          {this.links.map(link => (
+            <Button
+              icon={link.icon}
+              onClick={link.click}
+              style={{ width: "120px", margin: "5px" }}
+            >
+              {link.text}
+            </Button>
+          ))}
+        </div>
+        <Button
+          icon="logout"
+          onClick={() => this.props.logoutUser(() => Router.push("/"))}
+          type="danger"
+          style={{ width: "220px" }}
+        >
+          Logout
+        </Button>
+      </div>
+    );
 
-        return full;
-    }
+    return full;
+  }
 
-    render() {
-        return this.props.isAuthenticated ? this.renderLoggedIn() : this.renderNotLoggedIn();
-    }
+  render() {
+    return this.props.isAuthenticated
+      ? this.renderLoggedIn()
+      : this.renderNotLoggedIn();
+  }
 
-    links = [
-        { text: 'Bugs', icon: 'exclamation-circle' },
-        { text: 'Friends', icon: 'team' }
-    ];
+  links = [
+    { text: "Bugs", icon: "exclamation-circle" },
+    { text: "Friends", icon: "team" }
+  ];
 }
 
 const mapStateToProps = state => {
-    const { user, isAuthenticated, error } = state.auth;
-    //const { error, text } = state.errors;
-    return { user, isAuthenticated, error };
+  const { user, isAuthenticated, error } = state.auth;
+  //const { error, text } = state.errors;
+  return { user, isAuthenticated, error };
 };
 
-export default connect(mapStateToProps, null)(PlayerInfo);
+export default connect(
+  mapStateToProps,
+  null
+)(PlayerInfo);
